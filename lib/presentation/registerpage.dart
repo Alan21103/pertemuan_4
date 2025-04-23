adcd2c4d2d78903ca737d1bf9d304a8c0442fd4a
Alan21103 <syahlansaputra03@gmail.com>
2025-04-23 20:34:04 +0700

    kembali ke logika awal yang di myklass

--- lib/presentation/registerpage.dart
+++ lib/presentation/registerpage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pertemuan_4/presentation/loginpage.dart';
-
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -12,24 +10,21 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form(
-        key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('Register'),
+              Text('Register'),
               TextFormField(
                 controller: emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
@@ -52,27 +47,19 @@ class _RegisterPageState extends State<RegisterPage> {
                 },
               ),
               ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    Navigator.pop(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginPage(),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Register'),
+                onPressed: () {},
+                child: Text('Register'),
               ),
               TextButton(
                 onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
+                  Navigator.pushNamed(context, '/login');
+                  // Navigator.pop(context);
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const LoginPage()),
+                  // );
                 },
-                child: const Text('Sudah punya akun? Login Sekarang!'),
+                child: Text('Sudah punya akun? Login Sekarang!'),
               ),
             ],
           ),
