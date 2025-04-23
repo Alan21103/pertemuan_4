5fb56719c6e4a6499a959d0a2f57504b6caf5909
Alan21103 <syahlansaputra03@gmail.com>
2025-04-23 16:08:14 +0700

    Buat tampilan detail pesanan dan tombol untuk menyelesaikan pesanan

--- lib/presentation/detailorderpage.dart
+++ lib/presentation/detailorderpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pertemuan_4/presentation/homepage.dart';
 
 
 class DetailOrderPage extends StatelessWidget {
@@ -16,4 +17,37 @@ class DetailOrderPage extends StatelessWidget {
     required this.jumlahMinuman,
     required this.totalHarga,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
